3a0d1d8f0bfba4d75ccd10c49fc16179e3544171
lai0xn <onigenko12@gmail.com>
2024-08-10 20:34:11 +0100

    log

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,38 +1,43 @@
 package server
 
 import (
-	"log"
 	"net/http"
 	"time"
+
+	"github.com/charmbracelet/log"
 
 	"github.com/go-chi/chi/v5"
 )
 
 type Config struct {
 	ADDR string
+	Log  *log.Logger
 }
 
 type Server struct {
-	config Config
+	Addr   string
+	logger *log.Logger
 }
 
 func NewWithConfig(config Config) *Server {
 	return &Server{
-		config: config,
+		Addr:   config.ADDR,
+		logger: config.Log,
 	}
 }
 
 func (s *Server) Run() {
 	r := chi.NewRouter()
 	server := http.Server{
-		Addr:              s.config.ADDR,
+		Addr:              s.Addr,
 		Handler:           r,
 		ReadTimeout:       10 * time.Second,
 		WriteTimeout:      10 * time.Second,
 		IdleTimeout:       10 * time.Second,
 		ReadHeaderTimeout: 10 * time.Second,
 	}
+	s.logger.Info("Server Started Listening", "port", server.Addr)
 	if err := server.ListenAndServe(); err != nil {
-		log.Println(err)
+		s.logger.Error(err)
 	}
 }
