a15b3bda2dc17e9ba81a91176add9fa1e7495d86
lai0xn <onigenko12@gmail.com>
2024-09-08 20:23:46 +0100

    [fix]: non existing models

---
+++ internal/services/.null-ls_824645_workspace.go
@@ -0,0 +1,12 @@
+package services
+
+import "github.com/lai0xn/squid-tech/prisma"
+
+type WorkspaceService struct{}
+
+func NewWorkspaceService() *WorkspaceService {
+	return &WorkspaceService{}
+}
+
+func (s *WorkspaceService) CreateWorksapce() {
+}
---
+++ internal/services/workspace.go
@@ -0,0 +1,10 @@
+package services
+
+type WorkspaceService struct{}
+
+func NewWorkspaceService() *WorkspaceService {
+	return &WorkspaceService{}
+}
+
+func (s *WorkspaceService) CreateWorksapce() {
+}
