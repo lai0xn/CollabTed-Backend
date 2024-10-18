e5d2b284af3f353c4e9351a5e6eb2dadf20e0ed4
isif00 <sifodev@gmail.com>
2024-10-18 03:03:03 +0200

    [fix]: idk

--- internal/router/calls.go
+++ internal/router/calls.go
@@ -10,7 +10,9 @@ func CallsRoutes(e *echo.Group) {
 	h := handlers.NewCallHandler()
 
 	calls := e.Group("/calls", middlewares.AuthMiddleware)
-	calls.GET("/join/global/:workspaceId/:participantName", h.GetGlobalJoinToken)
-	calls.GET("/join/private/:workspaceId/:participantName/:receiverId", h.GetPrivatelJoinToken)
+	calls.GET("/create/global/:workspaceId/:participantName", h.GetGlobalJoinToken)
+	calls.GET("/create/private/:workspaceId/:participantName/:receiverId", h.GetPrivatelJoinToken)
+
+	calls.GET("/join/:roomId/:participantName", h.JoinRoomToken)
 
 }
--- internal/services/calls.go
+++ internal/services/calls.go
@@ -13,30 +13,53 @@ func NewCallService() *CallService {
 	return &CallService{}
 }
 
-func (s *CallService) GetGlobalJoinToken(participantName string, workspaceId string) (string, error) {
+func (s *CallService) GetGlobalJoinToken(participantName string, workspaceId string) (string, string, error) {
 	API_KEY := config.LIVEKIT_API_KEY
 	API_SECRET := config.LIVEKIT_API_SECRET
 
 	at := auth.NewAccessToken(API_KEY, API_SECRET)
 
+	var roomId string = uuid.NewString() + workspaceId
 	grant := &auth.VideoGrant{
 		RoomJoin: true,
-		Room:     "GlobalRoom" + workspaceId,
+		Room:     roomId,
 	}
 
 	at.AddGrant(grant).SetIdentity(participantName)
 
 	globalRoomJoinToken, err := at.ToJWT()
 	if err != nil {
-		return "", err
+		return "", "", err
 	}
 
 	logger.LogDebug().Msg(globalRoomJoinToken)
 
-	return globalRoomJoinToken, nil
+	return globalRoomJoinToken, roomId, nil
 }
 
-func (s *CallService) GetPrivateJoinToken(participantName string, workspaceId string) (string, error) {
+func (s *CallService) GetPrivateJoinToken(participantName string, workspaceId string) (string, string, error) {
+	API_KEY := config.LIVEKIT_API_KEY
+	API_SECRET := config.LIVEKIT_API_SECRET
+
+	at := auth.NewAccessToken(API_KEY, API_SECRET)
+
+	var roomId string = uuid.NewString() + workspaceId
+	grant := &auth.VideoGrant{
+		RoomJoin: true,
+		Room:     roomId,
+	}
+
+	at.AddGrant(grant).SetIdentity(participantName)
+
+	privateRoomJoinToken, err := at.ToJWT()
+	if err != nil {
+		return "", "", err
+	}
+
+	return privateRoomJoinToken, roomId, nil
+}
+
+func (s *CallService) JoinRoomToken(roomId string, participantName string) (string, error) {
 	API_KEY := config.LIVEKIT_API_KEY
 	API_SECRET := config.LIVEKIT_API_SECRET
 
@@ -44,7 +67,7 @@ func (s *CallService) GetPrivateJoinToken(participantName string, workspaceId st
 
 	grant := &auth.VideoGrant{
 		RoomJoin: true,
-		Room:     uuid.NewString() + workspaceId,
+		Room:     roomId,
 	}
 
 	at.AddGrant(grant).SetIdentity(participantName)
