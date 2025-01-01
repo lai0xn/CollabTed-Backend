f44b3d45aba8be0319c0cd122fef3428710d7e0b
lain <onigenko12@gmail.com>
2025-01-01 20:49:18 +0100

    replace empty spaces in avatar

--- internal/handlers/auth.go
+++ internal/handlers/auth.go
@@ -81,8 +81,8 @@ func (h *authHandler) Register(c echo.Context) error {
 
 	if payload.ProfilePicture == "" {
 		avatarURL := fmt.Sprintf(
-			strings.ReplaceAll("https://ui-avatars.com/api/?name=%s", " ", ""),
-			url.QueryEscape(payload.Name),
+			strings.ReplaceAll(fmt.Sprintf("https://ui-avatars.com/api/?name=%s",
+				url.QueryEscape(payload.Name)), " ", ""),
 		)
 
 		payload.ProfilePicture = avatarURL
